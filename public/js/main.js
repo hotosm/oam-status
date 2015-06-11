ffc888db95ee9801c2a8a217149ce5e174032b2a
Joe Flasher <joseph.flasher@gmail.com>
2015-06-11 12:04:19 -0400

    Using oam-catalog analytics endpoint

--- public/js/main.js
+++ public/js/main.js
@@ -4,14 +4,20 @@
 function reqListener () {
   var data = JSON.parse(this.responseText);
 
+  var stats = document.getElementsByClassName('stats')[0];
   // Set properties on holders
-  document.getElementsByClassName('images')[0].innerHTML = data.meta.found;
+  for (var i = 0; i < data.results.length; i++) {
+    var result = data.results[i];
+    var div = document.createElement('div');
+    div.innerHTML = result.count + ' images as of ' + result.date + '.';
+    stats.appendChild(div);
+  }
 
   // Show stats box now that we have stats
-  document.getElementsByClassName('stats')[0].style['display'] = 'block';
+  stats.style['display'] = 'block';
 }
 
 var oReq = new XMLHttpRequest();
 oReq.onload = reqListener;
-oReq.open('get', 'https://oam-catalog.herokuapp.com/', true);
+oReq.open('get', 'https://oam-catalog.herokuapp.com/analytics', true);
 oReq.send();
