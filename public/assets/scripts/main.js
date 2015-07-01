b3fdd04da8f259a70914551ca897a8ddd7f5f4f9
Daniel Silva <daniel@developmentseed.org>
2015-07-01 11:50:57 +0100

    Integrate new design with api

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -4,20 +4,28 @@
 function reqListener () {
   var data = JSON.parse(this.responseText);
 
-  var stats = document.getElementsByClassName('stats')[0];
+  var stats = document.querySelector('[data-hook*="history"] .stats-list');
   // Set properties on holders
   for (var i = 0; i < data.results.length; i++) {
     var result = data.results[i];
-    var div = document.createElement('div');
-    div.innerHTML = result.count + ' images as of ' + result.date + '.';
-    stats.appendChild(div);
-  }
+    var dateFromNow = moment(result.date).fromNow();
 
-  // Show stats box now that we have stats
-  stats.style['display'] = 'block';
+    if (i == 0) {
+      var last = document.querySelector('[data-hook*="last-update"]');
+      last.innerHTML = 'Updated ' + dateFromNow;
+    }
+
+    var dt = document.createElement('dt');
+    var dd = document.createElement('dd');
+
+    dt.innerHTML = result.count + ' images';
+    dd.innerHTML = dateFromNow;
+    stats.appendChild(dt);
+    stats.appendChild(dd);
+  }
 }
 
 var oReq = new XMLHttpRequest();
 oReq.onload = reqListener;
-oReq.open('get', 'https://oam-catalog.herokuapp.com/analytics', true);
+oReq.open('get', 'https://oam-catalog.herokuapp.com/analytics?limit=16', true);
 oReq.send();
